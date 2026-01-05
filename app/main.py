fa6823895646b4e643669bd988ccea0242285a2c
Kyra Web Studio <163783175+Kyraweb@users.noreply.github.com>
2026-01-05 02:01:57 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,19 @@ from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
+origins = [
+    "http://j04kwgsks88okgkgcwgcwkg8.142.171.41.4.sslip.io",  # frontend URL
+    "*",  # optional, allows all origins for testing
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/population")
 def population():
     try:
@@ -20,3 +33,4 @@ def population():
         return {"data": data}
     except Exception as e:
         return {"error": str(e)}
+
