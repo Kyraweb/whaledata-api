1f04e39a86038ee6ddbfd237e04d72a07232ce35
Kyra Web Studio <163783175+Kyraweb@users.noreply.github.com>
2026-01-05 00:47:05 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
-from .database import get_connection
+from app.database import get_connection
 from psycopg2.extras import RealDictCursor
 
-app = FastAPI(title="WhaleData API")
+app = FastAPI()
 
 @app.get("/population")
 def population():
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT species, population, ST_X(location::geometry) AS longitude, ST_Y(location::geometry) AS latitude, region, last_updated FROM whales;")
-    data = cur.fetchall()
-    cur.close()
-    conn.close()
-    return {"data": data}
-
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute("""
+            SELECT species, population,
+                   ST_X(location::geometry) AS longitude,
+                   ST_Y(location::geometry) AS latitude,
+                   region, last_updated
+            FROM whales;
+        """)
+        data = cur.fetchall()
+        return {"data": data}
+    except Exception as e:
+        return {"error": str(e)}
